82207e1df79c31923fd0b1898a44a85fd570443c
Aaron Pestel <ammppp@gmail.com>
2025-03-03 00:51:41 +0000

    .

--- deploy-gcs-trigger.sh
+++ deploy-gcs-trigger.sh
@@ -3,7 +3,8 @@
 gcloud functions deploy gcs-trigger-function \
     --region="us-central1" \
     --runtime="python312" \
-    --trigger-http \
+    --trigger-bucket="apestel" \
+    --trigger-location="us" \
     --entry-point="invoke" \
     --no-allow-unauthenticated \
     --source=./gcs-trigger
